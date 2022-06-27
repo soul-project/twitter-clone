5a2761526d86fe71e603c39fc06c60d295d031d9
lws803 <lws803@gmail.com>
2022-06-27 15:34:16 +0800

    necessary post endpoints for posts

--- src/pages/api/posts/[postId].ts
+++ src/pages/api/posts/[postId].ts
@@ -1,15 +1,19 @@
 import {
+  Body,
   createHandler,
   Delete,
   Get,
   HttpException,
+  Put,
   Req,
+  ValidationPipe,
 } from "@storyofams/next-api-decorators";
 import * as next from "next";
 import { Client } from "redis-om";
 import { StatusCodes } from "http-status-codes";
 
 import { postSchema } from "src/models/posts";
+import { CreatePostBodyDto } from "src/serializers/posts.dto";
 
 class PostHandler {
   redisClient: Client | undefined = undefined;
@@ -31,7 +35,26 @@ class PostHandler {
   async deletePost(@Req() { query: { postId } }: next.NextApiRequest) {
     const client = await this.getRedisClient();
     const postRepository = client.fetchRepository(postSchema);
+    // TODO: Validate to see if current user owns this post
     await postRepository.remove(postId as string);
+  }
+
+  @Put()
+  async updatePost(
+    @Req()
+    { query: { postId } }: next.NextApiRequest,
+    @Body(ValidationPipe) body: CreatePostBodyDto
+  ) {
+    const client = await this.getRedisClient();
+    const postRepository = client.fetchRepository(postSchema);
+    const existingPost = await postRepository.fetch(postId as string);
+    // TODO: Validate if current user owns this post
+
+    existingPost.body = body.body;
+    existingPost.title = body.title;
+
+    await postRepository.save(existingPost);
+    return existingPost;
   }
 
   private async getRedisClient() {
