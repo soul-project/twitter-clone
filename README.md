d7384fdf5926853db9dd971ae3ec6d025b7a2577
lws803 <lws803@gmail.com>
2022-07-12 13:17:06 +0800

    use in memory pouchdb instead

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Twitter clone developed with Soul Network as the authentication and identity provider.
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsoul-project%2Ftwitter-clone%2Ftree%2Fmain&env=SOUL_PLATFORM_ID,NEXTAUTH_SECRET,NEXTAUTH_URL)
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsoul-project%2Ftwitter-clone%2Ftree%2Fmain&env=SOUL_PLATFORM_ID,NEXTAUTH_SECRET,NEXTAUTH_URL,DB_NAME)
 
 ## Running locally
 
--- src/pages/api/common/base.controller.ts
+++ src/pages/api/common/base.controller.ts
@@ -1,14 +1,16 @@
 import { createRxDatabase, RxDatabase } from "rxdb";
-import { getRxStorageLoki } from "rxdb/plugins/lokijs";
+import { addPouchPlugin, getRxStoragePouch } from "rxdb/plugins/pouchdb";
 
 export class BaseController {
   rxClient: RxDatabase | undefined = undefined;
 
   async getRxDbClient() {
     if (!this.rxClient) {
+      addPouchPlugin(require("pouchdb-adapter-memory"));
+
       this.rxClient = await createRxDatabase({
-        name: "twitter-clone-db",
-        storage: getRxStorageLoki(),
+        name: process.env.DB_NAME!,
+        storage: getRxStoragePouch("memory"),
         ignoreDuplicate: true,
       });
     }
