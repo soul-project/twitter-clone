f87810d11fc35609c357e53d26d87ea2611cbede
lws803 <lws803@gmail.com>
2022-09-17 02:22:27 +0800

    remove `from_user_id` field from creating new connections

--- src/modules/userConnections/create.ts
+++ src/modules/userConnections/create.ts
@@ -4,7 +4,7 @@ import { Session } from "next-auth";
 export const create = async ({ toUserId, session }: CreateArgs) => {
   await axios.post(
     "https://api.soul-network.com/v1/user-connections",
-    { from_user_id: session.user.id, to_user_id: toUserId },
+    { to_user_id: toUserId },
     {
       headers: {
         "Content-Type": "application/json",
